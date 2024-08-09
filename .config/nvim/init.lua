a5dc217b5c9b16374f90b2f7875a84d15aa6d779
Boyd Thomas <boydbt48@gmail.com>
2024-08-08 19:38:41 -0700

    nvim keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,26 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = '\\'
-vim.o.expandtab = true -- ew, get those tabs away from me
+-- ew, get those tabs away from me
+vim.o.expandtab = true 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.number = true
-vim.o.background = 'dark' -- avoid blinding yourself
+-- avoid blinding yourself
+vim.o.background = 'dark' 
+-- file commands
+vim.keymap.set('n', '<leader>q', ':q<cr>')
+vim.keymap.set('n', '<leader>Q', ':q!<cr>')
+vim.keymap.set('n', '<leader>W', ':w<cr>')
+vim.keymap.set('n', '<leader>R', ':e!<cr>')
+-- movement commands
+vim.keymap.set('n', '<leader>w', '<C-W><up>')
+vim.keymap.set('n', '<leader>a', '<C-W><left>')
+vim.keymap.set('n', '<leader>s', '<C-W><down>')
+vim.keymap.set('n', '<leader>d', '<C-W><right>')
+-- tiling commands
+vim.keymap.set('n', '<leader><up>', '<C-W>v')
+vim.keymap.set('n', '<leader><left>', '<C-W>s')
+vim.keymap.set('n', '<leader><down>', '<C-W>v')
+vim.keymap.set('n', '<leader><right>', '<C-W>s')
 
 require("config.lazy")
